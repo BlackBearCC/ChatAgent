847ed5bf6bef1b7de0d0652fcd40d909975792c5
leozy0819 <leozy0819@gmail.com>
2023-12-01 18:08:39 +0800

    openai风格APi调用

--- simpleaichat/textgen.py
+++ simpleaichat/textgen.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 
 class TextGen:
@@ -9,7 +11,7 @@ class TextGen:
         """
         self.model_url = model_url
 
-    def generate(self, prompt: str, max_new_tokens: int = 250) -> str:
+    def generate(self, prompt: str, max_new_tokens: int = 300) -> str:
         """调用 textgen Web API 并返回输出。
 
         Args:
@@ -19,19 +21,64 @@ class TextGen:
         Returns:
             str: 生成的文本。
         """
-        url = f"{self.model_url}/api/v1/generate"
+        # url = f"{self.model_url}/api/v1/generate"
+        url = f"{self.model_url}/v1/chat/completions"
+        headers = {
+            "Content-Type": "application/json"
+        }
+        history = []
+
+        while True:
+            user_message = input("> ")
+            history.append({"role": "user", "content": user_message})
+            data = {
+                "mode": "chat",
+                "character": "Example",
+                "messages": history
+            }
+
+            response = requests.post(url, headers=headers, json=data, verify=False)
+            assistant_message = response.json()['choices'][0]['message']['content']
+            history.append({"role": "assistant", "content": assistant_message})
+            print(assistant_message)
+
         params = {
             "prompt": prompt,
             "max_new_tokens": max_new_tokens
         }
         response = requests.post(url, json=params)
-
+        print(response.json())
         if response.status_code == 200:
             return response.json()["results"][0]["text"]
         else:
             raise Exception(f"API 请求失败，状态码: {response.status_code}")
 
+
+url = "http://123.60.183.64:5001/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+history = []
+
+while True:
+    user_message = input("> ")
+    history.append({"role": "user", "content": user_message})
+    data = {
+        "mode": "chat",
+        "character": "Example",
+        "messages": history
+    }
+
+    response = requests.post(url, headers=headers, json=data, verify=False)
+    assistant_message = response.json()['choices'][0]['message']['content']
+    history.append({"role": "assistant", "content": assistant_message})
+    print(assistant_message)
+
 # 使用示例
-llm = TextGen(model_url="http://123.60.183.64:5000")
-result = llm.generate(prompt="写一个关于太空的故事。")
+llm = TextGen(model_url="http://123.60.183.64:5001")
+result = llm.generate(prompt="你吃饭了吗。")
 print(result)
+
+
